5e43a8e8133d71a77c5e2a48f814e6f273ff48e1
yann <me.yan.xia@qq.com>
2018-05-29 11:25:30 +0800

    Fuck it *3

--- unit_test/utils_test.cpp
+++ unit_test/utils_test.cpp
@@ -1,6 +1,6 @@
 
 #include <gtest/gtest.h>
-#include "utils.h"
+#include "utils.c"
 
 TEST (ALibraryTests, Print) {
     EXPECT_EQ(pasrse_ip_address("12.0.0.1"), 2);
@@ -8,4 +8,9 @@ TEST (ALibraryTests, Print) {
 
 TEST (ALibraryTests, Calculation) {
     EXPECT_EQ(4, 2 * 2);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest( &argc, argv );
+    return RUN_ALL_TESTS();
 }
